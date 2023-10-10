bbcbf3d4a84ec0f4d0fcb697c8d57c1b902e3b4c
Wooyoung Song <woosong@umich.edu>
2023-10-10 11:49:46 -0400

    Pack

---
+++ Pack.cpp
@@ -0,0 +1,63 @@
+#include "Card.hpp"
+#include <array>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "Pack.hpp"
+
+  // EFFECTS: Initializes the Pack to be in the following standard order:
+  //          the cards of the lowest suit arranged from lowest rank to
+  //          highest rank, followed by the cards of the next lowest suit
+  //          in order from lowest to highest rank, and so on.
+  // NOTE: The standard order is the same as that in pack.in.
+  // NOTE: Do NOT use pack.in in your implementation of this function
+  Pack::Pack(){
+    Card current;
+    for (int i = 0; i < PACK_SIZE; i++) {
+        Card c((Rank)(7 + i % 6), (Suit)(i/(PACK_SIZE / 4)));
+        cards[i] = c;
+    }
+  }
+
+  // REQUIRES: pack_input contains a representation of a Pack in the
+  //           format required by the project specification
+  // MODIFIES: pack_input
+  // EFFECTS: Initializes Pack by reading from pack_input.
+  Pack::Pack(std::istream& pack_input){
+    std::string s;
+    Card c;
+    int i = 0;
+    while (pack_input >> c) {
+        cards[i] = c;
+        i++;
+    }
+  }
+
+  // REQUIRES: cards remain in the Pack
+  // EFFECTS: Returns the next card in the pack and increments the next index
+  Card Pack::deal_one(){
+    Card c = cards[next];
+    next ++;
+    return c;
+  }
+
+  // EFFECTS: Resets next index to first card in the Pack
+  void Pack::reset(){
+    next = 0;
+  }
+
+  // EFFECTS: Shuffles the Pack and resets the next index. This
+  //          performs an in shuffle seven times. See
+  //          https://en.wikipedia.org/wiki/In_shuffle.
+  void Pack::shuffle(){
+    next = 0;
+    
+  }
+
+  // EFFECTS: returns true if there are no more cards left in the pack
+  bool Pack::empty() const{
+    if(next == 25){
+        return true;
+    }
+    return false;
+  }
