e1f6ca1cfe90556e68d346f9382beac864dbbc17
James <panzhenyu2014@gmail.com>
2023-10-10 11:20:48 -0400

    basic pack tests

--- Pack_tests.cpp
+++ Pack_tests.cpp
@@ -2,6 +2,7 @@
 #include "unit_test_framework.hpp"
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -12,6 +13,39 @@ TEST(test_pack_default_ctor) {
     ASSERT_EQUAL(SPADES, first.get_suit());
 }
 
-// Add more tests here
+TEST(test_pack_stream_ctor) {
+    const int pack_size = 24;
+    ifstream pack_file("pack.in");
+    Pack pack(pack_file);
+    Card current;
+    
+    for (int i = 0; i < pack_size; i++) {
+        current = pack.deal_one();
+        ASSERT_EQUAL(NINE + i % 6, current.get_rank());
+        ASSERT_EQUAL(i/(pack_size / 4), current.get_suit());
+    }
+}
+
+TEST(test_pack_reset) {
+    Pack pack; 
+    Card first = pack.deal_one();
+    pack.reset();
+    Card first2 = pack.deal_one();
+
+    ASSERT_EQUAL(first, first2);
+}
+
+TEST(test_pack_empty) {
+    const int pack_size = 24;
+    Pack pack;
+    Card current;
+    
+    for (int i = 0; i < pack_size; i++) {
+        current = pack.deal_one();
+    }
+
+    ASSERT_TRUE(pack.empty());
+}
+
 
 TEST_MAIN()
